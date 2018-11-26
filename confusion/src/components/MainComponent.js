58947c20a51d1049d726ee7a64876dba5e08e4b6
Vivek Gupta <c6vivek@gmail.com>
2018-11-25 19:52:30 -0600

    Single page applications part 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -19,13 +19,8 @@ class Main extends Component {
             dishes: DISHES,
             comments: COMMENTS,
             promotions: PROMOTIONS,
-            leaders: LEADERS,
-            selectedDish: null
+            leaders: LEADERS
         };
-    }
-
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
     }
 
     render() {
@@ -38,12 +33,21 @@ class Main extends Component {
                 />
             );
         }
+
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+            );
+        };
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
