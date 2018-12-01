efbcf8a5ec937bde9c58fde427ef7edb5654779c
Vivek Gupta <c6vivek@gmail.com>
2018-11-30 19:06:11 -0600

    Week 3 assignment

---
+++ confusion/src/components/CommentFormComponent.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleForm = this.toggleForm.bind(this);
+        this.state = {
+            isFormOpen: false
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleForm() {
+        this.setState({
+            isFormOpen: !this.state.isFormOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.toggleForm();
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Button onClick={this.toggleForm}><span className="fa fa-edit fa-lg"></span> Submit Comment</Button>
+                <Modal isOpen={this.state.isFormOpen} toggle={this.toggleForm}>
+                    <ModalHeader toggle={this.toggleForm}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Label htmlFor="rating">Rating</Label>
+                            <Control.select model=".rating" name="rating" id="rating"
+                                className="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Control.select>
+                            <br />
+
+                            <Label htmlFor="author">Your Name</Label>
+                            <Control.text model=".author" id="author" name="author"
+                                placeholder="Your Name"
+                                className="form-control"
+                                validators={{
+                                    required, minLength: minLength(3), maxLength: maxLength(15)
+                                }}
+                            />
+                            <Errors
+                                className="text-danger"
+                                model=".name"
+                                show="touched"
+                                messages={{
+                                    required: 'Required',
+                                    minLength: 'Must be greater than 2 characters',
+                                    maxLength: 'Must be 15 characters or less'
+                                }}
+                            />
+                            <br />
+
+                            <Label htmlFor="comment">Your Feedback</Label>
+                            <Control.textarea model=".comment" id="comment" name="comment"
+                                rows="6"
+                                className="form-control" />
+                            <br />
+
+                            <Button type="submit" color="primary">Submit</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}
+
+export default CommentForm;
--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentFormComponent';
 
 function RenderDish({ dish }) {
     return (
@@ -59,6 +60,7 @@ const DishDetail = (props) => {
                 </div>
                 <div className="col-12 col-md-5 m-1">
                     <RenderComments comments={props.comments} />
+                    <CommentForm />
                 </div>
             </div>
         </div>
